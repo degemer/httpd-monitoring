666992d0af33fca6b4aa025bc889ee3bdc7b8ac3
Quentin Madec <quentin.madec@student.ecp.fr>
2014-10-16 21:55:00 +0200

    Add test for Data

--- spec/unit/data_spec.rb
+++ spec/unit/data_spec.rb
@@ -1,10 +1,11 @@
 require_relative '../spec_helper'
 
 describe HttpdMonitoring::Data do
+  before(:all) { @time = Time.now.round(0) }
   let(:datas) do
     result = {}
     result[:host] = '64.242.88.10'
-    result[:date] = Time.now.round(0)
+    result[:date] = @time
     result[:path] = '/sec'
     result[:bytes] = 200
     result
@@ -12,14 +13,48 @@ describe HttpdMonitoring::Data do
   let(:data) { HttpdMonitoring::Data.new }
 
   describe '#insert' do
-    it 'updates all 10s datas'
+    it 'updates all 10s datas' do
+      data.insert(datas)
+      expect(data.instance_variable_get(:@sections_10s)).to eq('/sec' => 1)
+      expect(data.instance_variable_get(:@traffic_10s)).to eq(200)
+      expect(data.instance_variable_get(:@ips_10s)).to eq('64.242.88.10' => 1)
+    end
 
-    it 'updates all 2min datas'
+    it 'updates all 2min datas' do
+      data.insert(datas)
+      expect(data.instance_variable_get(:@last_time)).to eq(@time)
+      expect(data.instance_variable_get(:@hits_2min)).to eq(1)
+      expect(data.instance_variable_get(:@times_2min)).to eq(@time => 1)
+    end
   end
 
   describe '#select_data_10s' do
-    it 'returns all 10s datas'
+    it 'returns all 10s datas' do
+      data.insert(datas)
+      expect(data.select_data_10s).to eq(
+        sections: { '/sec' => 1 },
+        traffic: 200,
+        ips: { '64.242.88.10' => 1 }
+      )
+    end
 
-    it 'clears all 10s datas'
+    it 'clears all 10s datas' do
+      data.insert(datas)
+      data.select_data_10s
+      expect(data.instance_variable_get(:@sections_10s)).to eq({})
+      expect(data.instance_variable_get(:@traffic_10s)).to eq(0)
+      expect(data.instance_variable_get(:@ips_10s)).to eq({})
+    end
+  end
+
+  describe '#delete_old_2min' do
+    it 'clears data > 2 min' do
+      data.insert(datas)
+      datas[:date] += 60
+      data.insert(datas)
+      data.delete_old_2min(@time + 121)
+      expect(data.instance_variable_get(:@hits_2min)).to eq(1)
+      expect(data.instance_variable_get(:@times_2min)).to eq(@time + 60 => 1)
+    end
   end
 end
